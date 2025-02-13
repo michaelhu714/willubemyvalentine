538853fbfac3121c6c4a5e5612d9c60700d387a3
Michael Hu <michaelhu714@gmail.com>
2025-02-13 02:31:48 -0500

    5

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function handleNoClick() {
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.3}px`;
+    yesButton.style.fontSize = `${currentSize * 1.2}px`;
 }
 
 function handleYesClick() {
